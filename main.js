838f20ada115a9370651661d0923b1eb5f4a1d9f
Melyssa Moya <120610288+dntfindmel@users.noreply.github.com>
2023-01-11 21:17:20 -0300

    Unfinished

---
+++ main.js
@@ -0,0 +1,11 @@
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
