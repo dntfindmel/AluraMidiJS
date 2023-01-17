d9053c7564e7ef7ef9be8145c116d80e2f9cc5fb
Melyssa Moya <120610288+dntfindmel@users.noreply.github.com>
2023-01-16 21:59:30 -0300

    Finished project

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idAudioElement) {
+    document.querySelector(idAudioElement).play();
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++ ) {
+
+    const teclas = listaDeTeclas[contador];
+    const instrument = teclas.classList[1];
+    const idAudio = `#som_${instrument}`;
+
+    teclas.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    teclas.onkeydown = function(event) {
+
+        if (event.code === 'Space' || event.code === 'Enter') {
+            teclas.classList.add('ativa');
+        }
+
+    }
+
+
+
+    teclas.onkeyup = function () {
+        teclas.classList.remove('ativa');
+    }
+
 }
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
